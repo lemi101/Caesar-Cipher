fd514fcbc188045be08d44e4de196b2c76aae62b
Luthfi Maulana <30421448+lemi101@users.noreply.github.com>
2018-04-12 10:27:14 +0700

    Bulk Commit

---
+++ caesarcipher_encrypt.m
@@ -0,0 +1,41 @@
+%by lemi101
+
+addpath(pwd);
+function encrypt()
+  clc;
+  ln = "=================================================";
+  disp(ln);
+  disp("Encrypt Caesar Cipher");
+  disp(ln);
+  p = input("Masukkan Plaintext : ", 's');
+  k = input("Masukkan Key       : ");
+  
+  if(k < 1 || k > 26)
+    error("Key yang diperbolehkan berada pada rentang 1 - 25!!!");
+  endif
+  
+  ln = "=================================================";
+  printf("%s\n", ln);
+  printf("Plaintext = %s\nKey = %d\n", p, k);
+  a1 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  a2 = 'abcdefghijklmnopqrstuvwxyz';
+  x = 1;
+  
+  while (x <= length(p))
+    if(double(p(x)) > 64 && double(p(x)) < 91)
+      n = mod(mod((double(p(x)) + k + 26), 65), 26) + 1;
+      c(x) = a1(n);
+    elseif(double(p(x)) > 96 && double(p(x)) < 123)
+      n = mod(mod((double(p(x)) + k + 26), 97), 26) + 1;
+      c(x) = a2(n);
+    else
+      c(x) = double(p(x));
+    endif
+    x = x+1;
+  endwhile
+  c = char(c);
+  printf ("Ciphertext = %s\n", c);
+  printf("%s\n\n", ln);
+endfunction
+
+encrypt();
